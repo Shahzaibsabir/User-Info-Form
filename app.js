67ca5d8681781263ff0582b3616c36dd3248affe
Muhammad Shahzaib Khan <m.shahzaibsabir@gmail.com>
2023-10-22 20:11:00 +0500

    User Form Add

---
+++ app.js
@@ -0,0 +1,6 @@
+// let dataData = async () =>{
+//     const result = await fetch("https://api.github.com/users/qasim070")
+//     const data  = await result.json()
+//     console.log(data)
+// }
+// dataData()
